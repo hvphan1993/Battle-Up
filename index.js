f403a27a9e05966dfa7588110c67fb260c7e605a
hvphan1993 <hvphan1993@gmail.com>
2023-01-07 19:14:26 -0800

    added styling to ui, finished up game mechanics and look

--- index.js
+++ index.js
@@ -179,6 +179,8 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   mushroom.update();
+  c.fillStyle = ' rgba(255, 255, 255, 0.15)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
   player.update();
   enemy.update();
 
@@ -232,7 +234,9 @@ function animate() {
     enemy.takeHit();
     player.isAttacking = false;
 
-    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+    gsap.to('#enemyHealth', {
+      width: enemy.health + "%"
+    })
   }
 
   // if player misses
@@ -251,7 +255,10 @@ function animate() {
   ) {
     player.takeHit();
     enemy.isAttacking = false;
-    document.querySelector("#playerHealth").style.width = player.health + "%";
+    
+    gsap.to('#playerHealth', {
+      width: player.health + "%"
+    })
   }
 
   // if enemy misses
